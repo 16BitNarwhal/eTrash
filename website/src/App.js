c3e86045e8f4af799cb8bf20b651ab1dfed71095
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2022-04-08 20:31:22 -0400

    logo

--- website/src/App.js
+++ website/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img width="100" src="/logo.png" alt="logo" />
         <h1>eTrash</h1>
       </header>
       <section>
